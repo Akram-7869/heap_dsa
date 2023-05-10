485d45a9d00f6531d59b4ae171b9bef001ff5b5a
Akram-Sulthan <122307729+Akram-7869@users.noreply.github.com>
2023-05-10 19:50:22 +0530

    Update heap.java

--- priority Queue/heap.java
+++ priority Queue/heap.java
@@ -5,7 +5,7 @@ public class heap {
     static class heaps{
         static ArrayList<Integer>list=new ArrayList<>();
 
-        public static void add(int data){
+        private  void add(int data){
             list.add(data);
             int child=list.size()-1;
             int parent=(child-1)/2;
@@ -13,9 +13,63 @@ public class heap {
                 int temp=list.get(child);
                 list.set(child,list.get(parent));
                 list.set(parent, temp);
+                child=parent;
+                parent=(child-1)/2;
             }
-            
-
         }
+        //get the min heap
+        public int getmin(){
+            return list.get(0);
+        }
+
+        public void heapify(int i){
+            int left=(2*i)+1;
+            int right=(2*i)+2;
+            int mini=i;
+
+            if(left< list.size() && list.get(mini)>list.get(left))
+            mini=left;
+            if(right <list.size() && list.get(mini)>list.get(right))
+            mini=right;
+            if(mini!=i){
+                //swap
+                int temp=list.get(i);
+                list.set(i,list.get(mini));
+                list.set(mini,temp);
+               heapify(mini);
+            }
+        }
+        
+        //is empty
+        public boolean isempty(){
+            return list.size()==0;
+        }
+
+        
+        //remove the element
+        public int remove(){
+            int data=list.get(0);
+            //swap 
+            int temp=list.get(0);
+            list.set(0, list.get(list.size()-1));
+            list.set(list.size()-1,temp);
+
+            list.remove(list.size()-1);
+            heapify(0);
+            return data;
+        }
+
     }
+
+    public static void main(String[] args) {
+        heap h=new heap();
+        h.add(4);
+        h.add(5);
+        h.add(2);
+        h.add(1);
+        
+
+    }
+
+   
 }
